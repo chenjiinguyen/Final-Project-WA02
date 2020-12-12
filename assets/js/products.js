2a2f8b34b6d512ddbf20dcbb169919ef08855cbc
Ken-tech411 <71333435+Ken-tech411@users.noreply.github.com>
2020-12-12 19:02:57 +0700

    12-12-2020

---
+++ assets/js/products.js
@@ -0,0 +1,36 @@
+let products = [
+    {
+        id: "cyperpunk",
+        name: "Cyperpunk 2077",
+        price: 990000,
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/capsule_616x353.jpg?t=1607590317",
+
+    },
+    {
+        id: "minecraft",
+        name: "Minecraft",
+        price: 750000,
+        image: "https://hgeqic7azi.vcdn.com.vn/image/catalog/wallet/minecraft-produktbild-460x215.png",
+
+    }, {
+        id: "reddeadonline",
+        name: "Red Dead Online",
+        price: 124000,
+        image: "https://steamcdn-a.akamaihd.net/steam/apps/1404210/header.jpg ",
+    }, {
+        id: "reddeadonline",
+        name: "Red Dead Online",
+        price: 124000,
+        image: "https://steamcdn-a.akamaihd.net/steam/apps/1404210/header.jpg ",
+    }, {
+        id: "reddeadonline",
+        name: "Red Dead Online",
+        price: 124000,
+        image: "https://steamcdn-a.akamaihd.net/steam/apps/1404210/header.jpg ",
+    }, {
+        id: "reddeadonline",
+        name: "Red Dead Online",
+        price: 124000,
+        image: "https://steamcdn-a.akamaihd.net/steam/apps/1404210/header.jpg ",
+    },
+]
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,1 +1,39 @@
-let search = document.getElementById("searchbtn")
+let product_result = document.getElementById("products");
+const urlParams = new URLSearchParams(window.location.search);
+const formatter = new Intl.NumberFormat('it-IT')
+
+let search_keyword = urlParams.get("search")
+if(search_keyword != null && search_keyword.length > 0){
+    result = document.getElementById("result")
+    result.innerHTML = "<h2>Result for: " + search_keyword + "</h2>"
+    product_result.innerHTML = "";
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        if (product.name.search(search_keyword) >= 0) {
+    
+            element_product = document.createElement('div');
+            element_product.setAttribute("class", "product")
+            element_product.innerHTML = ` <div class="thumbnail-product">
+                                                <img src="${product.image}" />
+                                            </div>
+                                            <div class="info-product">
+                                                <div class="product-center">
+                                                    <h3>${product.name}</h3>
+                                                </div>
+                                                <div class="product-center">
+                                                    <h5>${formatter.format(product.price)} VND</h5>
+                                                </div>
+                                                <div class="product-center"><button>Mua Ngay</button></div>
+    
+                                            </div>`
+            product_result.appendChild(element_product)
+        }
+    }
+    if(product_result.innerHTML == ""){
+        alert = document.createElement('h2');
+        alert.innerHTML = "No result for: " + search_keyword
+        alert.setAttribute("class", "center");
+        product_result.appendChild(alert)
+    }
+}
+
